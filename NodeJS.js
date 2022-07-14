d8160f8ea54068570d3a7cc3dc7273216ce54a09
Nandini Gnanesh <nandini.gnanesh@exathought.com>
2022-07-14 17:57:38 +0530

    added event loop section

--- NodeJS.js
+++ NodeJS.js
@@ -89,4 +89,38 @@ console.log("Program Ended");
 The above example shows that the program does not wait for file reading and
 proceeds to print "Program Ended" and at the same time, the program without 
 blocking continues reading the file
+
+//-----------------------------------------------------------------------------------
+//                                 6. Event Loop
+//-----------------------------------------------------------------------------------
+
+// Import events module
+var events = require('events');
+
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+   console.log('connection succesful.');
+  
+   // Fire the data_received event 
+   eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+ 
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function() {
+   console.log('data received succesfully.');
+});
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+console.log("Program Ended.");
+
+
+
 */
