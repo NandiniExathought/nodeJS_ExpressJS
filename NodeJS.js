518e7075d053b5be9fceb443779babd8088f6fea
Nandini Gnanesh <nandini.gnanesh@exathought.com>
2022-07-14 16:34:12 +0530

    added Callbacks Concept section

--- NodeJS.js
+++ NodeJS.js
@@ -13,6 +13,9 @@ Node JS won't run in the browser , instead it runs as a standalone(like jvm).
 //-----------------------------------------------------------------------------------
 
 
+1. Node is neither a programming language nor a framework.
+2. Node is a runtime environment for executing Javascript code.
+
 //-----------------------------------------------------------------------------------
 //                                 3. Environment Setup
 //-----------------------------------------------------------------------------------
@@ -40,5 +43,50 @@ console.log('Server running at http://127.0.0.1:8081/');
 
 Command : node main.js
 
+//-----------------------------------------------------------------------------------
+//                                 5. Callbacks Concept
+//-----------------------------------------------------------------------------------
 
+In NodeJS, require() is a built-in function to include external modules that exist in separate files.
+require() statement basically reads a JavaScript file, executes it, and then proceeds to return the export object.
+
+Blocking code example :
+
+In Input.txt
+
+Tutorials Point is giving self learning content
+to teach the world in simple and easy way!!!!!
+
+-----------------------------------------------
+
+In main.js
+
+var fs = require("fs");
+var data = fs.readFileSync('input.txt');
+
+console.log(data.toString());
+console.log("Program Ended");
+
+cmd : node main.js
+
+The above example shows that the program blocks until it reads
+the file and then only it proceeds to end the program.
+----------------------------------------------------------------
+
+Non-blocking code example : 
+
+In main.js
+
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
+
+console.log("Program Ended");
+
+The above example shows that the program does not wait for file reading and
+proceeds to print "Program Ended" and at the same time, the program without 
+blocking continues reading the file
 */
