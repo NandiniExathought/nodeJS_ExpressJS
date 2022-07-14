376fec1f836179b801b34e04fd3f6fd6881d715f
Nandini Gnanesh <nandini.gnanesh@exathought.com>
2022-07-14 12:21:38 +0530

    add Home and Introduction section

--- NodeJS.js
+++ NodeJS.js
@@ -1,1 +1,23 @@
-console.log('Hello world');
+/*
+KeyPoints :
+Node JS won't run in the browser , instead it runs as a standalone(like jvm).
+
+
+//-----------------------------------------------------------------------------------
+//                                  Home
+//-----------------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------------
+//                                  Introduction
+//-----------------------------------------------------------------------------------
+
+1. What is Node.js?
+2. Features of Node.js
+3. Who Uses Node.js?
+4. Concepts
+5. Where to Use Node.js?
+6. Where Not to Use Node.js?
+
+
+*/
